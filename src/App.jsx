0892dd168ddc35af19cbbf5fda71792cc445d512
Egor Alexandrovich <bbff33333@gmail.com>
2023-11-23 14:23:22 +0200

    add 23.11

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
   return (
     <>
       <div className="wrapper">
+        <h1>123</h1>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
