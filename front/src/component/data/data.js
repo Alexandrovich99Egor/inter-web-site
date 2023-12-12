a3713e40513d9c5dcd24f208e1b2af00dfd39935
Egor Alexandrovich n <bbff33333@gmail.com>
2023-12-12 19:38:14 +0300

    changes 12.12

--- front/src/component/data/data.js
+++ front/src/component/data/data.js
@@ -1,182 +1,201 @@
-import ImageHtml from '../image/html.png';
-import ImageCss from '../image/css.png';
-import ImageJS from '../image/js.png';
-import ImagePy from '../image/py.png';
-import ImageMySql from '../image/mysqllogo.png'
-import ImageRoblox from '../image/roblox.png'
-import User1 from '../image/Mogilko-768x512.jpg';
-import User2 from '../image/Odrinskiy-768x535.jpg';
-import User3 from '../image/sofa-682x1024.jpg';
-import UserFeedback from '../image/14.jpg';
-import UserFeedback2 from '../image/18.jpg';
-import UserFeedback3 from '../image/52.jpg';
-import UserFeedback4 from '../image/avatar4.jpg';
-import UserFeedback5 from '../image/avatar39.jpg';
-import UserFeedback6 from '../image/avatar3.jpg';
-import UserFeedback7 from '../image/74.jpg';
-import UserFeedback8 from '../image/9.jpg';
-import UserFeedback9 from '../image/avatar2.jpg';
-
-
-
-
+import ImageHtml from "../image/html.png";
+import ImageCss from "../image/css.png";
+import ImageJS from "../image/js.png";
+import ImagePy from "../image/py.png";
+import ImageMySql from "../image/mysqllogo.png";
+import ImageRoblox from "../image/roblox.png";
+import User1 from "../image/Mogilko-768x512.jpg";
+import User2 from "../image/Odrinskiy-768x535.jpg";
+import User3 from "../image/sofa-682x1024.jpg";
+import UserFeedback from "../image/14.jpg";
+import UserFeedback2 from "../image/18.jpg";
+import UserFeedback3 from "../image/52.jpg";
+import UserFeedback4 from "../image/avatar4.jpg";
+import UserFeedback5 from "../image/avatar39.jpg";
+import UserFeedback6 from "../image/avatar3.jpg";
+import UserFeedback7 from "../image/74.jpg";
+import UserFeedback8 from "../image/sss.jpg";
+import UserFeedback9 from "../image/avatar2.jpg";
+import UserFeedback10 from "../image/ssss.jpg";
+import UserFeedback11 from "../image/sssss.png";
+import UserFeedback12 from "../image/ssssss.jpg";
+import UserFeedback13 from "../image/sssssss.jpg";
+import UserFeedback14 from "../image/aa.jpg";
+import UserFeedback15 from "../image/aaa.jpg";
+import UserFeedback16 from "../image/aaaaa.jpg";
+import UserFeedback17 from "../image/assass.jpg";
 export const data = [
-    {
-        id: 1,
-        title: 'HTML ',
-        age: '5-9 клас',
-        discriotion: 'HTML (Мова розмітки гіпертексту) - це мова, яка використовується для створення веб-сторінок. Вона визначає структуру та розмітку елементів на сторінці, таких як заголовки, параграфи, зображення, посилання тощо.',
-        image: ImageHtml
-    },
-    {
-        id: 2,
-        title: 'CSS',
-        age: '5-9 клас',
-        discriotion: 'CSS (Каскадні таблиці стилів) - це мова, яка використовується для оформлення та вигляду веб-сторінок, створюючи правила стилів для кольорів, шрифтів, відступів, розмірів та інших аспектів веб-елементів.',
-        image: ImageCss
-    },
-    {
-        id: 3,
-        age: '5-9 клас',
-        title: 'JavaScript Canvas',
-        discriotion: "JavaScript Canvas - це як магічний олівець для комп'ютера.Ти можеш намалювати на екрані будь- що, що уявляєш! За допомогою JS Canvas, ти створюєш свої власні світи та робиш їх живими.Наприклад, ти можеш створити ігру, де твої герої рухаються, чи навіть анімований мультфільм.",
-        image: ImageJS
-    },
-    {
-        id: 4,
-        age: '4-9 клас',
-        title: 'Python PHP',
-        discriotion: "PHP і Python - це дві потужні мови програмування, які використовуються для створення веб - сайтів та розробки програм.",
-        image: ImagePy
-    },
-    {
-        id: 5,
-        age: '6-9 клас',
-        title: 'MySQL',
-        discriotion: 'MySQL - це спеціальна програма для зберігання і управління даними. Замість того, щоб давати інструкції на зберігання іграшок чи книг, вона зберігає інформацію для веб-сайтів та програм. Так само, як ти складаєш свої іграшки у ящик, MySQL допомагає впорядковувати та знаходити дані для компютерних програм.',
-        image: ImageMySql
-    },
-    {
-        id: 5,
-        age: '6-9 клас',
-        title: 'Roblox',
-        discriotion: 'В айті - школі ми досліджуємо світ програмування за допомогою Роблокса! Тут ти зможеш створити власні ігри, додавати веселі функції та навіть розробляти свої власні історії.Забудь про границі - на Роблоксі можна втілити будь - які ідеї та зробити своє імя в світі розробників. Готуйся до захоплюючого подорожі у світ програмування!',
-        image: ImageRoblox
-
-    }
-
+  {
+    id: 1,
+    title: "HTML ",
+    age: "5-9 клас",
+    discriotion:
+      "HTML (Мова розмітки гіпертексту) - це мова, яка використовується для створення веб-сторінок. Вона визначає структуру та розмітку елементів на сторінці, таких як заголовки, параграфи, зображення, посилання тощо.",
+    image: ImageHtml,
+  },
+  {
+    id: 2,
+    title: "CSS",
+    age: "5-9 клас",
+    discriotion:
+      "CSS (Каскадні таблиці стилів) - це мова, яка використовується для оформлення та вигляду веб-сторінок, створюючи правила стилів для кольорів, шрифтів, відступів, розмірів та інших аспектів веб-елементів.",
+    image: ImageCss,
+  },
+  {
+    id: 3,
+    age: "5-9 клас",
+    title: "JavaScript Canvas",
+    discriotion:
+      "JavaScript Canvas - це як магічний олівець для комп'ютера.Ти можеш намалювати на екрані будь- що, що уявляєш! За допомогою JS Canvas, ти створюєш свої власні світи та робиш їх живими.Наприклад, ти можеш створити ігру, де твої герої рухаються, чи навіть анімований мультфільм.",
+    image: ImageJS,
+  },
+  {
+    id: 4,
+    age: "4-9 клас",
+    title: "Python PHP",
+    discriotion:
+      "PHP і Python - це дві потужні мови програмування, які використовуються для створення веб - сайтів та розробки програм.",
+    image: ImagePy,
+  },
+  {
+    id: 5,
+    age: "6-9 клас",
+    title: "MySQL",
+    discriotion:
+      "MySQL - це спеціальна програма для зберігання і управління даними. Замість того, щоб давати інструкції на зберігання іграшок чи книг, вона зберігає інформацію для веб-сайтів та програм. Так само, як ти складаєш свої іграшки у ящик, MySQL допомагає впорядковувати та знаходити дані для компютерних програм.",
+    image: ImageMySql,
+  },
+  {
+    id: 5,
+    age: "6-9 клас",
+    title: "Roblox",
+    discriotion:
+      "В айті - школі ми досліджуємо світ програмування за допомогою Роблокса! Тут ти зможеш створити власні ігри, додавати веселі функції та навіть розробляти свої власні історії.Забудь про границі - на Роблоксі можна втілити будь - які ідеї та зробити своє імя в світі розробників. Готуйся до захоплюючого подорожі у світ програмування!",
+    image: ImageRoblox,
+  },
 ];
 
 export const dataStudents = [
-    {
-        id: 1,
-        Name: 'Могилко Денис',
-        Image: User1,
-        Discriotion: ' мені 15 років і я живу і вчуся в місті Красноград, зараз активно вивчаю веб-програмування, працюю з мовою текстової розмітки HTML & CSS, створюю сайти за допомогою мови JS.'
-    },
+  {
+    id: 1,
+    Name: "Могилко Денис",
+    Image: User1,
+    Discriotion:
+      " мені 15 років і я живу і вчуся в місті Красноград, зараз активно вивчаю веб-програмування, працюю з мовою текстової розмітки HTML & CSS, створюю сайти за допомогою мови JS.",
+  },
 
-    {
-        id: 2,
-        Name: 'Одринський Богдан',
-        Image: User2,
-        Discriotion: 'мені 15 років я живу і вчуся в місті Краснограді/ Захоплююся вивченням мов програмування. Працюю і створюю сайти за допомогою мови текстової розмітки HTML & CSS і JS. Готовий взяти замовлення на розробку сайту.'
-    },
+  {
+    id: 2,
+    Name: "Одринський Богдан",
+    Image: User2,
+    Discriotion:
+      "мені 15 років я живу і вчуся в місті Краснограді/ Захоплююся вивченням мов програмування. Працюю і створюю сайти за допомогою мови текстової розмітки HTML & CSS і JS. Готовий взяти замовлення на розробку сайту.",
+  },
 
-    {
-        id: 3,
-        Name: 'Софія',
-        Image: User3,
-        Discriotion: ' Я опанувала HTML та такі мови програмування, як: JavaScript, CSS – почала вивчати Python. Вмію користуватися бібліотекою jQueri. Мої хоббі: програмування та малювання.'
-    }
-]
+  {
+    id: 3,
+    Name: "Софія",
+    Image: User3,
+    Discriotion:
+      " Я опанувала HTML та такі мови програмування, як: JavaScript, CSS – почала вивчати Python. Вмію користуватися бібліотекою jQueri. Мої хоббі: програмування та малювання.",
+  },
+];
 
 export const dataFeedBack = [
-    {
-        Name: 'Марія, 32 роки:',
-        Discriotion: '"Школа Inter-Web - відмінна платформа для моїх дітей! Вони навчилися створювати власні ігри на Roblox та ефективно використовувати PHP та MySQL. Вражена їх розвитком!"',
-        Avatar: UserFeedback
-
-    },
-    {
-        Name: 'Ігор, 14 років:',
-        Discriotion: '"Школа Inter-Web - відмінна платформа для моїх дітей! Вони навчилися створювати власні ігри на Roblox та ефективно використовувати PHP та MySQL. Вражена їх розвитком!"',
-        Avatar: UserFeedback2
-    },
-    {
-        Name: 'Тетяна, 38 років:',
-        Discriotion: '"Мої діти закохані в навчання в Inter-Web. Вони освоїли PHP та MySQL настільки ефективно, що вже розробляють власні програми. Це дійсно вражаюче!"',
-        Avatar: UserFeedback3
-    },
-    {
-        Name: 'Олексій, 16 років:',
-        Discriotion: '"Інтерактивні уроки з Roblox дали мені величезний заряд енергії! Inter-Web - краще місце для вивчення програмування та грифічного дизайну."',
-        Avatar: UserFeedback4
-    },
-    {
-        Name: 'Наталія, 29 років:',
-        Discriotion: '"Мої діти вчаться тут і неймовірно задоволені. Їм вдалося розкрити свій творчий потенціал завдяки урокам HTML та CSS. Рекомендую Inter-Web всім батькам!"',
-        Avatar: UserFeedback5
-    },
-    {
-        Name: 'Володимир, 13 років:',
-        Discriotion: '"PHP та MySQL були складними, але цікавими! Inter-Web допоміг мені зрозуміти, як створювати свої власні веб-сайти. Неймовірно круто!"',
-        Avatar: UserFeedback6
-    },
-    {
-        Name: 'Ольга, 35 років:',
-        Discriotion: '"Моя донька просто захоплена уроками в Inter-Web. Вивчаючи HTML та CSS, вона вже розробила власний блог. Тут справжні професіонали викладають!"',
-        Avatar: UserFeedback7
-    },
-    {
-        Name: 'Андрій, 15 років:',
-        Discriotion: '"Roblox - це щось неймовірне! Уроки в Inter-Web з цієї гри допомогли мені зрозуміти, як створювати власні веселі світи. Дякую за можливість!"',
-        Avatar: UserFeedback8
-    },
-    {
-        Name: 'Ірина, 40 років:',
-        Discriotion: '"Син захоплюється навчанням в Inter-Web. Я вражена, як він легко оволодіває різними технологіями - від HTML до MySQL. Професійні викладачі роблять свою справу чудово!"',
-        Avatar: UserFeedback9
-    },
-    {
-        Name: 'Владислав, 17 років:',
-        Discriotion: '"Інтерактивні уроки з PHP в Inter-Web - це саме те, що мені потрібно було для розвитку своїх програмістських навичок. Рекомендую!"',
-        Avatar: UserFeedback8
-
-    },
-    {
-        Name: 'Тетяна, 33 роки:',
-        Discriotion: '"Мій син вже вміє створювати власні ігри на Roblox завдяки курсам в Inter-Web. Я вражена його досягненнями. Дякуємо за теплу атмосферу!"',
-        Avatar: UserFeedback8
-
-    },
-    {
-        Name: 'Максим, 14 років:',
-        Discriotion: '"Школа Inter-Web - відмінна платформа для моїх дітей! Вони навчилися створювати власні ігри на Roblox та ефективно використовувати PHP та MySQL. Вражена їх розвитком!"',
-        Avatar: UserFeedback8
-
-    },
-    {
-        Name: 'Анастасія, 31 рік:',
-        Discriotion: '"Моя донька вже знає, як працювати з базами даних за допомогою MySQL, навчаючись в Inter-Web. Розвиваюча програма та професійні викладачі - найкращі!"',
-        Avatar: UserFeedback8
-
-    },
-    {
-        Name: 'Євген, 16 років:',
-        Discriotion: '"Roblox - це круто! Навчання в Inter-Web допомогло мені створити власний геймплей та збільшити свої навички програмування. Рекомендую!"',
-        Avatar: UserFeedback8
-
-    },
-    {
-        Name: 'Маргарита, 37 років:',
-        Discriotion: '"Школа Inter-Web дала моєму синові можливість реалізувати свої творчі ідеї. HTML, CSS, PHP - він вже вправний у цих технологіях!"',
-        Avatar: UserFeedback8
-
-    },
-    {
-        Name: 'Олександр, 18 років:',
-        Discriotion: '"Навчання в Inter-Web дало мені шанс поглибити знання в галузі PHP та MySQL. Тепер я готовий до викликів сучасного світу програмування',
-        Avatar: UserFeedback8
-
-    }
-]
+  {
+    Name: "Марія, 32 роки:",
+    Discriotion:
+      '"Школа Inter-Web - відмінна платформа для моїх дітей! Вони навчилися створювати власні ігри на Roblox та ефективно використовувати PHP та MySQL. Вражена їх розвитком!"',
+    Avatar: UserFeedback,
+  },
+  {
+    Name: "Ігор, 14 років:",
+    Discriotion:
+      '"Школа Inter-Web - відмінна платформа для моїх дітей! Вони навчилися створювати власні ігри на Roblox та ефективно використовувати PHP та MySQL. Вражена їх розвитком!"',
+    Avatar: UserFeedback2,
+  },
+  {
+    Name: "Тетяна, 38 років:",
+    Discriotion:
+      '"Мої діти закохані в навчання в Inter-Web. Вони освоїли PHP та MySQL настільки ефективно, що вже розробляють власні програми. Це дійсно вражаюче!"',
+    Avatar: UserFeedback3,
+  },
+  {
+    Name: "Олексій, 16 років:",
+    Discriotion:
+      '"Інтерактивні уроки з Roblox дали мені величезний заряд енергії! Inter-Web - краще місце для вивчення програмування та грифічного дизайну."',
+    Avatar: UserFeedback4,
+  },
+  {
+    Name: "Наталія, 29 років:",
+    Discriotion:
+      '"Мої діти вчаться тут і неймовірно задоволені. Їм вдалося розкрити свій творчий потенціал завдяки урокам HTML та CSS. Рекомендую Inter-Web всім батькам!"',
+    Avatar: UserFeedback5,
+  },
+  {
+    Name: "Володимир, 13 років:",
+    Discriotion:
+      '"PHP та MySQL були складними, але цікавими! Inter-Web допоміг мені зрозуміти, як створювати свої власні веб-сайти. Неймовірно круто!"',
+    Avatar: UserFeedback6,
+  },
+  {
+    Name: "Ольга, 35 років:",
+    Discriotion:
+      '"Моя донька просто захоплена уроками в Inter-Web. Вивчаючи HTML та CSS, вона вже розробила власний блог. Тут справжні професіонали викладають!"',
+    Avatar: UserFeedback7,
+  },
+  {
+    Name: "Андрій, 15 років:",
+    Discriotion:
+      '"Roblox - це щось неймовірне! Уроки в Inter-Web з цієї гри допомогли мені зрозуміти, як створювати власні веселі світи. Дякую за можливість!"',
+    Avatar: UserFeedback8,
+  },
+  {
+    Name: "Ірина, 40 років:",
+    Discriotion:
+      '"Син захоплюється навчанням в Inter-Web. Я вражена, як він легко оволодіває різними технологіями - від HTML до MySQL. Професійні викладачі роблять свою справу чудово!"',
+    Avatar: UserFeedback10,
+  },
+  {
+    Name: "Владислав, 17 років:",
+    Discriotion:
+      '"Інтерактивні уроки з PHP в Inter-Web - це саме те, що мені потрібно було для розвитку своїх програмістських навичок. Рекомендую!"',
+    Avatar: UserFeedback11,
+  },
+  {
+    Name: "Тетяна, 33 роки:",
+    Discriotion:
+      '"Мій син вже вміє створювати власні ігри на Roblox завдяки курсам в Inter-Web. Я вражена його досягненнями. Дякуємо за теплу атмосферу!"',
+    Avatar: UserFeedback12,
+  },
+  {
+    Name: "Максим, 14 років:",
+    Discriotion:
+      '"Школа Inter-Web - відмінна платформа для моїх дітей! Вони навчилися створювати власні ігри на Roblox та ефективно використовувати PHP та MySQL. Вражена їх розвитком!"',
+    Avatar: UserFeedback13,
+  },
+  {
+    Name: "Анастасія, 31 рік:",
+    Discriotion:
+      '"Моя донька вже знає, як працювати з базами даних за допомогою MySQL, навчаючись в Inter-Web. Розвиваюча програма та професійні викладачі - найкращі!"',
+    Avatar: UserFeedback14,
+  },
+  {
+    Name: "Євген, 16 років:",
+    Discriotion:
+      '"Roblox - це круто! Навчання в Inter-Web допомогло мені створити власний геймплей та збільшити свої навички програмування. Рекомендую!"',
+    Avatar: UserFeedback15,
+  },
+  {
+    Name: "Маргарита, 37 років:",
+    Discriotion:
+      '"Школа Inter-Web дала моєму синові можливість реалізувати свої творчі ідеї. HTML, CSS, PHP - він вже вправний у цих технологіях!"',
+    Avatar: UserFeedback16,
+  },
+  {
+    Name: "Олександр, 18 років:",
+    Discriotion:
+      '"Навчання в Inter-Web дало мені шанс поглибити знання в галузі PHP та MySQL. Тепер я готовий до викликів сучасного світу програмування',
+    Avatar: UserFeedback17,
+  },
+];
